cc46aacb38417d3f6dd58e4a5c2fb70a72ac2397
wuz1mu <kingaal93@gmail.com>
2023-06-20 12:14:11 +0700

    Generate base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, OrderModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
